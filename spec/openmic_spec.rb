13f6bc9665dcbd33732da8818c0305c14a72923c
Erika Kischuk <erika.kischuk@gmail.com>
2021-07-14 17:37:31 -0600

    Adjust setup and test instance

--- spec/openmic_spec.rb
+++ spec/openmic_spec.rb
@@ -1,17 +1,20 @@
 require './lib/joke'
 require './lib/user'
-require './lib/openmic'
+require './lib/open_mic'
 require 'pry'
 
 RSpec.describe OpenMic do
   before(:each) do
-    open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
-    user_1 = User.new("Sal")
-    user_2 = User.new("Ali")
-    joke_1 = Joke.new(22, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
-    joke_2 = Joke.new(13, "How do you keep a lion from charging?", "Take away its credit cards.")
+    @open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
+    @user_1 = User.new("Sal")
+    @user_2 = User.new("Ali")
+    @joke_1 = Joke.new(22, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    @joke_2 = Joke.new(13, "How do you keep a lion from charging?", "Take away its credit cards.")
   end
 
   describe 'Open Mic' do
+    it 'is an instance of OpenMic' do
+      expect(@open_mic).to be_a(OpenMic)
+    end
   end
 end
