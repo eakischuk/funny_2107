b3ce893c8283b35b3291610bf0ec99d68b55944f
Erika Kischuk <erika.kischuk@gmail.com>
2021-07-14 17:37:53 -0600

    Create OpenMic class with argument

--- lib/open_mic.rb
+++ lib/open_mic.rb
@@ -1,22 +1,8 @@
+require './lib/joke'
+require './lib/user'
+
 class OpenMic
-  attr_reader :location,
-              :date,
-              :performers
-
-  def initialize(attributes)
-    @location = attributes[:location]
-    @date = attributes[:date]
-    @performers = []
+  def initialize(information)
   end
 
-  def welcome(user)
-    @performers << user
-  end
-
-  def repeated_jokes?
-    jokes = @performers.map do |user|
-      user.jokes
-    end.flatten
-    jokes.length != jokes.uniq.length
-  end
 end
