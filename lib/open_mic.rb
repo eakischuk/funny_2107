210433ff512a9e18c24b56115644b37b6e8e0048
Erika Kischuk <erika.kischuk@gmail.com>
2021-07-14 17:44:32 -0600

    Finalize basic OpenMic class

--- lib/open_mic.rb
+++ lib/open_mic.rb
@@ -2,7 +2,12 @@ require './lib/joke'
 require './lib/user'
 
 class OpenMic
+  attr_reader :date, :location, :performers
+
   def initialize(information)
+    @location = information[:location]
+    @date = information[:date]
+    @performers = []
   end
 
 end
